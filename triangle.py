8918014f3b2387ced5136019099128a8c317fbcd
Vigen Oganesyan <38381948+ArmenianButcher@users.noreply.github.com>
2018-07-16 23:39:21 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,75 +1,25 @@
 from OpenGL.GLUT import *
-from OpenGL.GLU import *
 from OpenGL.GL import *
-from OpenGL.GL import shaders
-import numpy as np
-
-
-VERTEX_SHADER = """
-#version 330
-
-in vec4 position;
-
-void main(){
-
-gl_Position = position;
-}
-"""
-
-FRAGMENT_SHADER = """
-#version 330
-
-void main(){
-
-gl_FragColor = vec4(1.0,0.0,0.0,1.0);
-}
-
-"""
-
-shaderProgram = None
-
-def Initialize():
-	global VERTEX_SHADER
-	global FRAGMENT_SHADER
-	global shaderProgram
-	
-	vertexshader = shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER)
-	fragmentshader = shaders.compileShader(FRAGMENT_SHADER, GL_FRAGMENT_SHADER)
-	shaderProgram = shaders.compileShader(vertexshader, fragmentshader)
-	
-	triangles = [-0.5,-0.5,0.0,
-	             0.5,-0.5,0.0,
-	             0.0,0.5,0.0]
-	triangles = np.array(triangles, dtype = np.float32)
-	VBO = glGenBuffers(1)
-	glBindBuffer(GL_ARRAY_BUFFER, VBO)
-	glBufferData(GL_ARRAY_BUFFER, triangles.nbytes, triangles, GL_STATIC_DRAW)
-	
-	position = glGetAttribLocation(shaderProgram, "position")
-	glVertexAttribPointer(position, 3,GL_FLOAT, GL_FALSE, 0,None)
-	glEnableVertexAttribArray(position)
-
-
 def render():
-	glClearColor(0,0,0,1)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-	
-	glUseProgram(shaderProgram)
-	glDrawArrays(GL_TRIANGLES,0,3)
-	glUseProgram(0)
-	
+	glBegin(GL_TRIANGLES)
+	glColor3f(1,0,0)
+	glVertex2f(-0.5, -0.5)
+	glColor3f(0,1,0)
+	glVertex2f(0.5, 0.0)
+	glColor3f(0,0,1)
+	glVertex2f(0.0, 0.0)
+	glEnd()
 	glutSwapBuffers()
 	
 def main():
 	glutInit()
-	glutInitContextVersion(3,3)
+	glutInitDisplayMode(GLUT_RGBA)
 	glutInitWindowSize(800,600)
 	glutInitWindowPosition(200,200)
-	glutCreateWindow("Tetris")
-	Initialize()
-	glutDisplayFunc(render())
+	glutCreateWindow("Tetris in future")
+	glutDisplayFunc(render)
+	glClearColor(0,0,0,0)
 	glutMainLoop()
-	
-	
-if __name__ == "__main__":
-	main()
+
+main()
